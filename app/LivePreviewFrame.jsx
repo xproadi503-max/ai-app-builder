2127cbfbc47b2049171a659ea39e3ff334faa4c6
xproadi503-max <xproadi503@gmail.com>
2026-08-17 08:20:05 +0530

    Include Tailwind CSS in LivePreviewFrame
    Added Tailwind CSS script to LivePreviewFrame.

--- app/LivePreviewFrame.jsx
+++ app/LivePreviewFrame.jsx
@@ -61,6 +61,7 @@ export default function LivePreviewFrame({ code: rawCode }) {
 <head>
   <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
   <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
   <style>body { margin: 0; font-family: sans-serif; }</style>
 </head>
 <body>
